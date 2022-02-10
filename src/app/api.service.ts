f7e5f7d12fcdd872c042281baef51bdc5c9e67b6
Harshvardhan <harshvardhanpatel2212@gmail.com>
2022-02-10 14:55:10 +0530

    Commit_1 Added an error handling container in output component

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -12,33 +12,44 @@ export class ApiService {
   
   display1 : boolean = false ;
   display2 : boolean = false ;
+  display3 : boolean = false ;
   responseRepositeries: any;
   responseForkers: any;
 
   constructor(public http: HttpClient) { }
 
   getRepositery() {
+    this.display1 = false ;
     this.display2 = false ;
+    this.display3 = false ;
     console.log("Getting Repositery from " + 'https://api.github.com/orgs/' + this.inputs[0] + "/repos?per_page=" +this.inputs[1]);
     this.http.get('https://api.github.com/orgs/' + this.inputs[0] + "/repos?per_page=" +this.inputs[1]).subscribe(
       (response) => {
         this.responseRepositeries = response ;
         console.log(this.responseRepositeries);
+        this.display1 = true ;
       },
-      (error) => { console.log(error); });
-      this.display1 = true ;
+      (error) => {
+        console.log(error);
+        this.display3 = true ;
+      });
   }
 
   getForkers(url: string) {
     this.display1 = false ;
+    this.display2 = false ;
+    this.display3 = false ;
     console.log("Getting Forkers from " + url  + "?per_page=" +this.inputs[2]);
     this.http.get(url  + "?per_page=" +this.inputs[2]).subscribe(
       (response) => {
         this.responseForkers = response ;
         console.log(this.responseForkers);
+        this.display2 = true ;
       },
-      (error) => { console.log(error); });
-    this.display2 = true ;
+      (error) => {
+        console.log(error);
+        this.display3 = true ;
+      });
   }
 
   viewRepositery(url:string) {
